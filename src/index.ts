21b01b329ba2878a349d59d84e5e92245530a53b
Evan Tseng-Jen Tseng <evan@tseng.io>
2025-08-30 03:03:16 +0800

    Extract Redis client setup into Redis helper

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
-import { createClient, RedisClientType } from "redis";
-import { Client as DiscordClient, GatewayIntentBits, TextChannel } from "discord.js";
-import dotenv from "dotenv";
-import { addTask, waitForResults } from "./redis-helper.js";
+import { Client, GatewayIntentBits, TextChannel } from "discord.js";
+import { addTask, generateClient, waitForResults } from "./redis-helper.js";
 
-dotenv.config();
-
-const REDIS_OPTIONS = {
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-  }
-}
-
-const redisTaskClient: RedisClientType = createClient(REDIS_OPTIONS);
-const redisResultClient: RedisClientType = createClient(REDIS_OPTIONS);
-const discordClient = new DiscordClient({
+const redisTaskClient = await generateClient();
+const redisResultClient = await generateClient();
+const discordClient = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
   ],
 });
-
-redisTaskClient.on("error", (err) => console.error("Redis Task Client Error", err));
-redisResultClient.on("error", (err) => console.error("Redis Result Client Error", err));
-await redisTaskClient.connect();
-await redisResultClient.connect();
 
 discordClient.on("clientReady", () => {
   waitForResults(redisResultClient, async (message) => {
