746a1561e264097386e202c1565ef658f219602f
Evan Tseng-Jen Tseng <evan@tseng.io>
2025-09-25 12:47:12 +0800

    Add tests for messageCreate event

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,30 +1,36 @@
-import { Client } from "discord.js";
+import { Client, Message } from "discord.js";
 
-const mockOnce = jest.fn();
 const mockFetchDiscordMembers = jest.fn();
 const mockFetchDiscordMessages = jest.fn();
+const mockAddRequestToStream = jest.fn();
 const mockListenForRedisResults = jest.fn().mockResolvedValue(undefined);
+const mockIsInvalidMessage = jest.fn();
 const mockClientInstance = {
-  once: mockOnce,
+  once: jest.fn(),
   on: jest.fn(),
   login: jest.fn(),
-  user: { tag: "test-bot" },
-} as unknown as Client;
+  user: { tag: "test-bot", id: "test-bot-id" },
+};
+const consoleErrorSpy = jest
+  .spyOn(console, "error")
+  .mockImplementation(() => {});
 
 jest.mock("../src/utils/discord", () => ({
   fetchDiscordMembers: mockFetchDiscordMembers,
   fetchDiscordMessages: mockFetchDiscordMessages,
+  isInvalidMessage: mockIsInvalidMessage,
 }));
 jest.mock("../src/utils/redis", () => ({
   createRedisClient: jest.fn(),
   listenForRedisResults: mockListenForRedisResults,
+  addRequestToStream: mockAddRequestToStream,
 }));
 jest.mock("../src/utils/server", () => ({
   startServer: jest.fn(),
 }));
 jest.mock("discord.js", () => ({
   Client: jest.fn().mockImplementation(() => {
-    return mockClientInstance;
+    return mockClientInstance as unknown as Client;
   }),
   GatewayIntentBits: {
     Guilds: 0,
@@ -51,7 +57,7 @@ describe("main", () => {
 
   describe("clientReady event", () => {
     it("should setup client ready listener, trigger it, and call the correct functions", async () => {
-      const callback = getMockListener(mockOnce, "clientReady");
+      const callback = getMockListener(mockClientInstance.once, "clientReady");
       expect(callback).toBeDefined();
 
       await callback(mockClientInstance);
@@ -59,6 +65,60 @@ describe("main", () => {
       expect(mockFetchDiscordMembers).toHaveBeenCalledTimes(1);
       expect(mockFetchDiscordMessages).toHaveBeenCalledTimes(1);
       expect(mockListenForRedisResults).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("messageCreate event", () => {
+    it("should ignore invalid messages", async () => {
+      mockIsInvalidMessage.mockReturnValue(true);
+      const callback = getMockListener(mockClientInstance.on, "messageCreate");
+      expect(callback).toBeDefined();
+
+      const message = {} as Message;
+
+      await callback(message);
+
+      expect(mockAddRequestToStream).not.toHaveBeenCalled();
+    });
+
+    it("should process valid messages", async () => {
+      mockIsInvalidMessage.mockReturnValue(false);
+      mockAddRequestToStream.mockResolvedValue(undefined);
+      const callback = getMockListener(mockClientInstance.on, "messageCreate");
+      expect(callback).toBeDefined();
+
+      const message = {
+        react: jest.fn(),
+      } as unknown as Message;
+
+      await callback(message);
+
+      expect(mockAddRequestToStream).toHaveBeenCalledWith(
+        undefined,
+        "messageCreate",
+        message,
+      );
+      expect(message.react).toHaveBeenCalledWith("🐾");
+    });
+
+    it("should handle errors when processing a message", async () => {
+      mockIsInvalidMessage.mockReturnValue(false);
+      const errorMessage = "Redis is down";
+      mockAddRequestToStream.mockRejectedValue(new Error(errorMessage));
+      const callback = getMockListener(mockClientInstance.on, "messageCreate");
+      expect(callback).toBeDefined();
+
+      const message = {
+        react: jest.fn(),
+        reply: jest.fn(),
+      } as unknown as Message;
+
+      await callback(message);
+
+      expect(message.reply).toHaveBeenCalledWith(
+        `Could not process your request: ${errorMessage}`,
+      );
+      expect(consoleErrorSpy).toHaveBeenCalled();
     });
   });
 });
